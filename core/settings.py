c0185adbe2413f7f69a1213de16c87583b12ba89
👨‍💻 Mathias Elorm <mathiaselorm@gmail.com>
2025-10-02 13:39:49 +0000

    updated core/settings.py to use the correct database configuration.

--- core/settings.py
+++ core/settings.py
@@ -113,11 +113,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 # Password validation
